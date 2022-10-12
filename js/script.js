96a1a71dcb564b760e7a931310617f8b2cc504f0
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-10-12 14:10:29 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* Created by: Alex.Nelson
+ * Created on: Oct 2022
+ * This file contains the JavaScript for index.html
+ */
+
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello World!</p>"
+}
